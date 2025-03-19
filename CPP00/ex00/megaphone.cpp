b390e56cf0d81e980653e3850d98e68f047d54e5
lyricallll <lachariotte30@gmail.com>
2025-03-19 13:00:57 +0100

    start cpp

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agraille <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 08:00:52 by agraille          #+#    #+#             */
+/*   Updated: 2025/03/19 09:26:21 by agraille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
+	for (int i = 1; argv[i]; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)std::toupper(argv[i][j]);
+	}
+	std::cout << "\n";
+	return 0;
+}
