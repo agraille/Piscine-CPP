4d534d8e8deca99be47e254d7fef8cc580544c74
lyricallll <lachariotte30@gmail.com>
2025-04-21 16:11:33 +0200

    pqtch parsing bitocin

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -9,11 +9,33 @@ static int stringToInt(const std::string& str){
 	return	result;
 }
 
-static bool	is_valid_file(const std::string& line_read){
-	if (line_read[4] != '-' && line_read[7] !=  '-' && line_read[10] != ',' && !isdigit(line_read[11]))
+static bool	check_date(const std::string& line_read){
+	if (line_read[4] != '-' || line_read[7] !=  '-' || line_read[10] != ',' || !isdigit(line_read[11]))
 		return false;
-	//verifier la date,
-	//verifier le float
+	short 			years = stringToInt(line_read.substr(0, 4));
+	short 			month = stringToInt(line_read.substr(5, 2));
+	short 			day = stringToInt(line_read.substr(8, 2));
+	if ((years < 2009 || years > 2025) || (month < 1 || month > 12) || (day < 1 || day > 31))
+		return false;
+	if ((day > 28 && month == 2) || (day > 30 && (month == 4 || month == 6 || month == 9 || month == 11)))
+		return false;
+	return true;
+}
+
+static bool	is_valid_data(const std::string& line_read){
+	if (line_read[10] != ',' || !isdigit(line_read[11]) || !check_date(line_read))
+		return false;
+
+	std::string 	value = line_read.substr(11);
+	short			point = 0;
+	for (int i = 0; value[i]; i++){
+		if (value[i] != '.' && !isdigit(value[i]))
+			return false;
+		if (value[i] == '.')
+			++point;
+	}
+	if (point > 1)
+		return false;
 	return true;
 }
 
@@ -21,11 +43,10 @@ bool	addDataToMap(std::ifstream& data){
 	std::map<int, std::string>	dataBase;
 	std::string				line_read;
 	getline(data, line_read);
-	if (line_read != "date,exchange_rate"){
+	if (line_read != "date,exchange_rate")
 		return false;
-	}
 	while (getline(data, line_read)){
-		if (is_valid_file(line_read) == false)
+		if (is_valid_data(line_read) == false)
 			return false;
 		line_read.erase(std::remove(line_read.begin(), line_read.end(), '-'), line_read.end());
 		int	key = stringToInt(line_read.substr(0, 8));
