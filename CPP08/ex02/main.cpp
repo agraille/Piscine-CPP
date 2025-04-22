e8bd1c4045f7edad7c0df43022b37b653b9d3f9d
lyricallll <lachariotte30@gmail.com>
2025-04-22 07:40:50 +0200

    start RPN

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -31,16 +31,17 @@ int main() {
 	mstack.push(17);
 	std::cout << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	// std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
 	std::cout << "Stack size: " << mstack.size() << std::endl;
 	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = std::prev(mstack.end());
+	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+	--ite;
 	while (ite != it)
 	{
 		std::cout << *ite << std::endl;
---
+++ CPP09/ex01/main.cpp
@@ -0,0 +1,10 @@
+#include "RPN.hpp"
+
+int	main(int argc, char** argv){
+	if (argc != 2){
+		std::cout << "Error: invalid arguments. Please read the subject instructions.\n";
+		return 1;
+	}
+
+	return 0;
+}
